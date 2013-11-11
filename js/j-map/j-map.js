79e58d1261796c6c85cbb8d2497ee8fe10797c92
Bohdan <bohdan.anderson@gmail.com>
2013-11-11 15:44:32 -0500

    some changes

--- js/j-map/j-map.js
+++ js/j-map/j-map.js
@@ -295,7 +295,7 @@ for more reference look at <a href="http://google-maps-utility-library-v3.google
 **/
 	this.useCluster = function(cOptions){
 		var clusterSettings = $.extend({
-			imagePath: '/static/img/base-',
+			imagePath: 'http://alsocollective.github.io/static/img/base-',
 			imageSizes: [100,100],
 			averageCenter:true
 		}, cOptions);
